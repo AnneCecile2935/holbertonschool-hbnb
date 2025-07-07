c9960489868f60414234bf6eebec12ce31cd5445
Helvlaska <ccastan@live.fr>
2025-07-07 11:10:30 +0200

    :hammer: (repository.py): Add SQLAlchemyRepository class for database operations with CRUD methods

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -24,6 +24,7 @@ Classes:
     Mainly used for testing or lightweight prototypes without a database.
 """
 from abc import ABC, abstractmethod
+# from app.models import User, Place, Review, Amenity
 
 
 class Repository(ABC):
@@ -194,4 +195,37 @@ class InMemoryRepository(Repository):
     def clear(self):
         """Clear all data from the in-memory storage."""
         self._storage.clear()
+
+class SQLAlchemyRepository(Repository):
+    def __init__(self, model):
+        self.model = model
+
+    def add(self, obj):
+        from app import db
+        db.session.add(obj)
+        db.session.commit()
+
+    def get(self, obj_id):
+        return self.model.query.get(obj_id)
+
+    def get_all(self):
+        return self.model.query.all()
+
+    def update(self, obj_id, data):
+        from app import db
+        obj = self.get(obj_id)
+        if obj:
+            for key, value in data.items():
+                setattr(obj, key, value)
+            db.session.commit()
+
+    def delete(self, obj_id):
+        from app import db
+        obj = self.get(obj_id)
+        if obj:
+            db.session.delete(obj)
+            db.session.commit()
+
+    def get_by_attribute(self, attr_name, attr_value):
+        return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
 
