64ec66aefe3a7f8936e42d1ba29f15e35757df66
Helvlaska <ccastan@live.fr>
2025-07-07 11:11:40 +0200

    :hammer: (user_repository.py): Add class UserRepository for the CRUD operations for users

---
+++ part3/app/persistence/repositories/user_repository.py
@@ -0,0 +1,14 @@
+from app.models.user import User
+from part3.app.extensions import db, bcrypt, jwt
+from app.persistence.repository import SQLAlchemyRepository
+
+class UserRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(User)
+
+    def get_user_by_email(self, email):
+        return self.model.query.filter_by(email=email).first()
+
+    def get_admin_users(self):
+        """Get all users with admin rights."""
+        return self.model.query.filter_by(is_admin=True).all()
